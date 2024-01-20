934a3a5d2ae57423c0b21254c13ce60a28c23e30
Luciano Loma Lorenzana <lucianolomal@gmail.com>
2024-01-20 01:00:22 -0800

    Updating repo

---
+++ backend/plateGenerator.py
@@ -0,0 +1,15 @@
+from menuItem import menuItemObj
+from foodPlate import plateObj
+
+"""
+mealTime: represent either Breakfast, Lunch, or Dinner
+desiredCalrs: represents the desired amount of calories
+"""
+
+
+def plateGeneratorFcn(mealTime, desiredCalrs):
+    return
+
+
+def userFunction(mealTime, desiredCalrs):
+    return
