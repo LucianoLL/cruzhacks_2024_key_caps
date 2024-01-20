fabf9cf96ae4d15cc58b684862202dd3bb4c46be
Luciano Loma Lorenzana <lucianolomal@gmail.com>
2024-01-19 22:57:40 -0800

    Made access functions

--- backend/testingFile.py
+++ backend/testingFile.py
@@ -2,3 +2,6 @@ from menuItem import menuItemObj
 
 item1 = menuItemObj("Steamed Rice", 144, ["Vegan", "Gluten Friendly"])
 print(item1)
+print(item1.getName())
+print(item1.getCalories())
+print(item1.getDietRestr())
